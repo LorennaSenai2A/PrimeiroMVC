c75303595851a4805ce06cf03f843a9793be57b0
LorennaSenai2A <lorennasenaia@gmail.com>
2024-08-27 14:27:49 -0300

    Finalizando o exemplo de lista de alunos

--- PrimeiroMVC/Controllers/AlunoController.cs
+++ PrimeiroMVC/Controllers/AlunoController.cs
@@ -5,14 +5,14 @@ namespace PrimeiroMVC.Controllers
 {
     public class AlunoController : Controller
     {
-        private object listaAlunos;
+
 
         public IActionResult Index()
         {
             Aluno a1 = new Aluno(1, "Lorena", "lorena.victor@aluno.senai.br", 121212);
             Aluno a2 = new Aluno(2, "Juliana", "juliana.fulanetto@aluno.senai.br", 909090);
             Aluno a3 = new Aluno(3, "Maysa", "maysa.rondon@aluno.senai.br", 454545);
-            Aluno a4 = new Aluno(4, "Matheus", "matheus.dias@aluno.senai.br", 707070);
+            Aluno a4 = new Aluno(4, "Md", "matheus.dias@aluno.senai.br", 707070);
             Aluno a5 = new Aluno(5, "Mavi", "maria.vitoria@aluno.senai.br", 262626);
 
             //Criar uma lista de alunos
